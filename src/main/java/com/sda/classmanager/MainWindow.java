06f93055ec360cae410504be6120b6b137a8c786
amen <pawel.reclaw@gmail.com>
2020-08-16 14:21:00 +0200

    Left panel extracted

--- src/main/java/com/sda/classmanager/MainWindow.java
+++ src/main/java/com/sda/classmanager/MainWindow.java
@@ -24,92 +24,49 @@ public class MainWindow extends JFrame implements ActionListener {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Pawel
         panel1 = new JPanel();
-        labelHeadlineForm = new JLabel();
-        hSpacer1 = new JPanel(null);
-        labelName = new JLabel();
-        textFieldName = new JTextField();
-        labelLastName = new JLabel();
-        textFieldLastName = new JTextField();
-        labelQuarantined = new JLabel();
-        checkQuarantined = new JCheckBox();
-        labelGender = new JLabel();
-        comboGender = new JComboBox();
-        labelYearBorn = new JLabel();
-        spinnerYearBorn = new JSpinner();
-        vSpacer4 = new JPanel(null);
-        buttonSubmit = new JButton();
-        vSpacer5 = new JPanel(null);
-        vSpacer1 = new JPanel(null);
-        vSpacer2 = new JPanel(null);
-        vSpacer6 = new JPanel(null);
         panel2 = new JPanel();
-        labelResult = new JLabel();
+        labelList = new JLabel();
+        scrollPane1 = new JScrollPane();
+        list1 = new JList();
 
         //======== this ========
-        Container contentPane = getContentPane();
+        var contentPane = getContentPane();
         contentPane.setLayout(new GridLayout(1, 2));
 
         //======== panel1 ========
         {
-            panel1.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
-            0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
-            .BOTTOM,new java.awt.Font("D\u0069alog",java.awt.Font.BOLD,12),java.awt.Color.
-            red),panel1. getBorder()));panel1. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
-            beans.PropertyChangeEvent e){if("\u0062order".equals(e.getPropertyName()))throw new RuntimeException();}});
+            panel1.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+            EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER, javax. swing
+            . border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ),
+            java. awt. Color. red) ,panel1. getBorder( )) ); panel1. addPropertyChangeListener (new java. beans. PropertyChangeListener( )
+            { @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName () ))
+            throw new RuntimeException( ); }} );
             panel1.setLayout(new GridLayout(12, 3, 1, 1));
-
-            //---- labelHeadlineForm ----
-            labelHeadlineForm.setText("Student Form:");
-            labelHeadlineForm.setFont(new Font("Bitstream Vera Sans", Font.BOLD, 21));
-            panel1.add(labelHeadlineForm);
-            panel1.add(hSpacer1);
-
-            //---- labelName ----
-            labelName.setText("Name:");
-            panel1.add(labelName);
-            panel1.add(textFieldName);
-
-            //---- labelLastName ----
-            labelLastName.setText("Last name:");
-            panel1.add(labelLastName);
-            panel1.add(textFieldLastName);
-
-            //---- labelQuarantined ----
-            labelQuarantined.setText("Quarantined:");
-            panel1.add(labelQuarantined);
-            panel1.add(checkQuarantined);
-
-            //---- labelGender ----
-            labelGender.setText("Gender:");
-            panel1.add(labelGender);
-            panel1.add(comboGender);
-
-            //---- labelYearBorn ----
-            labelYearBorn.setText("Year born:");
-            panel1.add(labelYearBorn);
-
-            //---- spinnerYearBorn ----
-            spinnerYearBorn.setModel(new SpinnerNumberModel(1920, 1920, 2020, 1));
-            panel1.add(spinnerYearBorn);
-            panel1.add(vSpacer4);
-
-            //---- buttonSubmit ----
-            buttonSubmit.setText("Submit");
-            panel1.add(buttonSubmit);
-            panel1.add(vSpacer5);
-            panel1.add(vSpacer1);
-            panel1.add(vSpacer2);
-            panel1.add(vSpacer6);
         }
         contentPane.add(panel1);
 
         //======== panel2 ========
         {
-            panel2.setLayout(new GridLayout(1, 1));
+            panel2.setLayout(new GridBagLayout());
+            ((GridBagLayout)panel2.getLayout()).columnWidths = new int[] {0, 0};
+            ((GridBagLayout)panel2.getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+            ((GridBagLayout)panel2.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+            ((GridBagLayout)panel2.getLayout()).rowWeights = new double[] {1.0, 1.0, 0.0, 1.0E-4};
 
-            //---- labelResult ----
-            labelResult.setText("text");
-            panel2.add(labelResult);
+            //---- labelList ----
+            labelList.setText("Student List:");
+            labelList.setFont(new Font("Bitstream Vera Sans", Font.BOLD, 21));
+            panel2.add(labelList, new GridBagConstraints(0, 0, 1, 1, 0.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
+
+            //======== scrollPane1 ========
+            {
+                scrollPane1.setViewportView(list1);
+            }
+            panel2.add(scrollPane1, new GridBagConstraints(0, 1, 1, 1, 0.0, 10.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
         }
         contentPane.add(panel2);
         pack();
@@ -117,40 +74,15 @@ public class MainWindow extends JFrame implements ActionListener {
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
 
         // Prepare Combobox with available options (Genders)
-        Gender[] genders = Gender.values();
-        for (Gender gender : genders) {
-            comboGender.addItem(gender);
-        }
-
-        // Prepare spinner Year Born                        .. Wartość początkowa    minimum             maksimum               skok
-        spinnerYearBorn.setModel(new SpinnerNumberModel(LocalDate.now().getYear(), 1920, LocalDate.now().getYear(), 1));
-
-        buttonSubmit.addActionListener(this);
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Pawel
     private JPanel panel1;
-    private JLabel labelHeadlineForm;
-    private JPanel hSpacer1;
-    private JLabel labelName;
-    private JTextField textFieldName;
-    private JLabel labelLastName;
-    private JTextField textFieldLastName;
-    private JLabel labelQuarantined;
-    private JCheckBox checkQuarantined;
-    private JLabel labelGender;
-    private JComboBox comboGender;
-    private JLabel labelYearBorn;
-    private JSpinner spinnerYearBorn;
-    private JPanel vSpacer4;
-    private JButton buttonSubmit;
-    private JPanel vSpacer5;
-    private JPanel vSpacer1;
-    private JPanel vSpacer2;
-    private JPanel vSpacer6;
     private JPanel panel2;
-    private JLabel labelResult;
+    private JLabel labelList;
+    private JScrollPane scrollPane1;
+    private JList list1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 
     @Override
@@ -164,6 +96,6 @@ public class MainWindow extends JFrame implements ActionListener {
 
 //        System.out.println(student);
 
-        labelResult.setText(student.toString());
+//        labelResult.setText(student.toString());
     }
 }
---
+++ src/main/java/com/sda/classmanager/StudentForm.java
@@ -0,0 +1,112 @@
+/*
+ * Created by JFormDesigner on Sun Aug 16 14:08:18 CEST 2020
+ */
+
+package com.sda.classmanager;
+
+import java.awt.*;
+import java.time.LocalDate;
+import javax.swing.*;
+
+/**
+ * @author Pawel
+ */
+public class StudentForm extends JPanel {
+    public StudentForm() {
+        initComponents();
+
+        Gender[] genders = Gender.values();
+        for (Gender gender : genders) {
+            comboGender.addItem(gender);
+        }
+        // Prepare spinner Year Born                        .. Wartość początkowa    minimum             maksimum               skok
+        spinnerYearBorn.setModel(new SpinnerNumberModel(LocalDate.now().getYear(), 1920, LocalDate.now().getYear(), 1));
+
+//        buttonSubmit.addActionListener();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Pawel
+        labelHeadlineForm = new JLabel();
+        hSpacer1 = new JPanel(null);
+        labelName = new JLabel();
+        textFieldName = new JTextField();
+        labelLastName = new JLabel();
+        textFieldLastName = new JTextField();
+        labelGender = new JLabel();
+        comboGender = new JComboBox();
+        labelQuarantined = new JLabel();
+        checkQuarantined = new JCheckBox();
+        labelYearBorn = new JLabel();
+        spinnerYearBorn = new JSpinner();
+        vSpacer5 = new JPanel(null);
+        buttonSubmit = new JButton();
+
+        //======== this ========
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(
+        0,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder
+        .BOTTOM,new java.awt.Font("D\u0069al\u006fg",java.awt.Font.BOLD,12),java.awt.Color.
+        red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.
+        beans.PropertyChangeEvent e){if("\u0062or\u0064er".equals(e.getPropertyName()))throw new RuntimeException();}});
+        setLayout(new GridLayout(12, 2));
+
+        //---- labelHeadlineForm ----
+        labelHeadlineForm.setText("Student Form:");
+        labelHeadlineForm.setFont(new Font("Bitstream Vera Sans", Font.BOLD, 21));
+        add(labelHeadlineForm);
+        add(hSpacer1);
+
+        //---- labelName ----
+        labelName.setText("Name:");
+        add(labelName);
+        add(textFieldName);
+
+        //---- labelLastName ----
+        labelLastName.setText("Last name:");
+        add(labelLastName);
+        add(textFieldLastName);
+
+        //---- labelGender ----
+        labelGender.setText("Gender:");
+        add(labelGender);
+        add(comboGender);
+
+        //---- labelQuarantined ----
+        labelQuarantined.setText("Quarantined:");
+        add(labelQuarantined);
+        add(checkQuarantined);
+
+        //---- labelYearBorn ----
+        labelYearBorn.setText("Year born:");
+        add(labelYearBorn);
+
+        //---- spinnerYearBorn ----
+        spinnerYearBorn.setModel(new SpinnerNumberModel(1920, 1920, 2020, 1));
+        add(spinnerYearBorn);
+        add(vSpacer5);
+
+        //---- buttonSubmit ----
+        buttonSubmit.setText("Submit");
+        add(buttonSubmit);
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Pawel
+    private JLabel labelHeadlineForm;
+    private JPanel hSpacer1;
+    private JLabel labelName;
+    private JTextField textFieldName;
+    private JLabel labelLastName;
+    private JTextField textFieldLastName;
+    private JLabel labelGender;
+    private JComboBox comboGender;
+    private JLabel labelQuarantined;
+    private JCheckBox checkQuarantined;
+    private JLabel labelYearBorn;
+    private JSpinner spinnerYearBorn;
+    private JPanel vSpacer5;
+    private JButton buttonSubmit;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
